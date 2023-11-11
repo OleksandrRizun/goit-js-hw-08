35ed129da9fdf7307945c55f368c5a3a0611ec0a
OleksandrRizun <137624723+OleksandrRizun@users.noreply.github.com>
2023-11-12 01:15:09 +0200

    ver 1 for checkup

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+import {saveToLS, loadFromLS} from './helpers';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle (saveCurrentTime, 1000));
+
+var currentTime = loadFromLS ("videoplayer-current-time");
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
+function saveCurrentTime (data){
+    const key = "videoplayer-current-time";
+    const value = data.seconds;
+    saveToLS (key, value);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+import {saveToLS, loadFromLS} from './helpers';
+
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener("input", throttle (onInput, 500));
+
+function onInput () {
+    let email = form.elements.email.value;
+    let message = form.elements.message.value;
+    const obj = {
+        email,
+        message
+    };
+    saveToLS ("feedback-form-state", obj)
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let email = form.elements.email.value;
+    let message = form.elements.message.value;
+    const obj = {
+        email,
+        message
+    };
+    console.log (obj);
+    localStorage.removeItem("feedback-form-state");
+    form.reset();
+});
+
+function onLoad () {
+    const fromLoadFromLS = loadFromLS("feedback-form-state") || {};
+    form.elements.email.value = fromLoadFromLS.email || "";
+    form.elements.message.value = fromLoadFromLS.message || "";
+}
+onLoad ();
